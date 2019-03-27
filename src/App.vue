88fea5d1d3567dc13bfdf8569cac3cad15615c33
Mayank Gangwal <mayankgangwal@Mayanks-MacBook-Air.local>
2019-03-27 23:52:36 +0530

    initial header component setup

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <router-view />
+  <div>
+    <RRHeaderComponent />
+    <div id="app">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import { Component } from 'vue-property-decorator'
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
 
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+
+import RRHeaderComponent from "./components/RRHeaderComponent.vue";
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld,
+    RRHeaderComponent
   }
 })
-export default class App extends Vue {
-
-}
+export default class App extends Vue {}
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/RRHeaderComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="headerBg">
+      <span id="headerLogo">
+          Header!
+      </span>
+      <span v-if="login" id="profilePic">
+          <i class="fas fa-user-circle"></i>
+      </span>
+  </div>
+</template>
+
+<<script>
+export default {
+  name: 'RRHeaderComponent',
+  data: function(){
+        return {
+            login: true
+        }
+
+  }
+}
+</script>
+
+
+<<style>
+    #headerBg {
+        background: red;
+        padding: 20px;
+        background: #52b983;
+        border-bottom: 1px solid #989EA4;
+        border-top: 1px solid #717D85;
+        color: #FFF;
+        margin: 0;
+        position: -webkit-sticky;
+        position: sticky;
+        top: -1px;
+    }
+    #profilePic {
+        font-size: 30px;
+        margin-right: 20px;
+        float: right;
+    }
+    #headerLogo {
+        font-size: 25px;
+    }
+</style>
