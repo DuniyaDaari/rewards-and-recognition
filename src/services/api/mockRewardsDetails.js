125cbf89723b7b32da8d576d4246120446345cfc
Tagadpallewar <P10479142@capita.co.uk>
2019-03-28 18:20:57 +0530

    added rewards-details service and mock json

---
+++ src/services/api/mockRewardsDetails.js
@@ -0,0 +1,30 @@
+export let mockRewardsDetails = [{
+  id: '123',
+  employeePid: 'P123456',
+  rewardId: '123456',
+  teamId: '789',
+  nominationDate: '12/03/1019',
+  nominatedBy: 'Mr. Abc',
+  comments: 'Excellenct lkjfoijs fkhdapfi',
+  rewardPoints: '1000',
+  createdBy: '',
+  createdDate: '',
+  lastModifiedBy: '',
+  lastModifiedDate: '',
+  name: 'Pat On The Back'
+}, {
+  id: '123',
+  employeePid: 'P1240456',
+  rewardId: '123454',
+  teamId: '789',
+  nominationDate: '13/03/1019',
+  nominatedBy: 'Mr. Abc xyz',
+  comments: 'Excellenct lk jfoijs fkhdapfi',
+  rewardPoints: '1000',
+  createdBy: '',
+  createdDate: '',
+  lastModifiedBy: '',
+  lastModifiedDate: '',
+  name: 'Pat On The Back'
+}
+]
