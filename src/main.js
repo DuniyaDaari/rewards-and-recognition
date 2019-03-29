230667c492a7e96c0ccdd0ffad4bcac9544d4221
Gangwal <P10484490@capita.co.uk>
2019-03-29 09:47:30 +0530

    imported bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import 'reflect-metadata'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 import { registerConstantValue, ROUTER, STORE } from './di'
 import { createStore } from './store'
