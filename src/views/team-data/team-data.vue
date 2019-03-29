a7b8529db8c9eea85ff1eb945a677ad8e464645a
Gangwal <P10484490@capita.co.uk>
2019-03-29 22:12:45 +0530

    modal popup for rewards

--- src/views/team-data/team-data.vue
+++ src/views/team-data/team-data.vue
@@ -17,7 +17,12 @@
           <td>{{employee.email}}</td>
           <td>{{employee.dateOfJoining}}</td>
           <td>
-            <span v-for="reward in employee.rewards" :key="reward.id">
+            <span
+              v-for="reward in employee.rewards"
+              :key="reward.id"
+              data-toggle="modal"
+              data-target="#rewardModal"
+            >
               <button
                 type="button"
                 class="btn btn-primary rewardButton"
@@ -26,6 +31,30 @@
                 :title="reward.rewardName"
                 :disabled="!reward.eligible"
               >{{reward.rewardName.substring(0, 1)}}</button>
+              <div
+                class="modal fade"
+                id="rewardModal"
+                tabindex="-1"
+                role="dialog"
+                aria-labelledby="exampleModalCenterTitle"
+                aria-hidden="true"
+              >
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">...</div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </span>
           </td>
         </tr>
