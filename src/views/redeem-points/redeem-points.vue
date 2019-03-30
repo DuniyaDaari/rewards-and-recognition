c5e8545c1bff1cc93f0e79938ee07bb324da1b97
Tagadpallewar <P10479142@capita.co.uk>
2019-03-30 17:54:27 +0530

    Added validation to redeem points

--- src/views/redeem-points/redeem-points.vue
+++ src/views/redeem-points/redeem-points.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
+    <div v-show="showSuccessMessage " class="success my-4">Successfully Redeemed {{redeemPoints}} Points </div>
     <form class="form-row redeem-form">
       <div class="col-md-3">
           <img class="paytm-img" :src="paytmImg" />
       </div>
       <div class="form-group col-md-3">
         <label for="points">Enter No. Of Points</label>
-        <input type="number" :max="totalRewardPoints" class="form-control" id="points" placeholder="Enter No. Of Points"/>
+        <input type="number" v-model="redeemPoints" :keyup="isPointsAvailable()" :max="totalRewardPoints" class="form-control" id="points" placeholder="Enter No. Of Points"/>
         <span>Available Points : </span>{{totalRewardPoints}}
+        <div v-if="isInValidPoints" class="error">Please enter valid value</div>
       </div>
       <div class="form-group col-md-3">
         <label for="mobileNo">Mobile No.</label>
-        <input type="number" class="form-control" id="mobileNo" placeholder="Mobile no.">
+        <input type="number" v-model="mobileNo" class="form-control" id="mobileNo" placeholder="Mobile no.">
+        <div v-if="isInValidMobileNo" class="error">Please Enter Valid Mobile No.</div>
       </div>
       <div class="col-md-3">
-        <button type="button" class="btn btn-primary btn-lg mt-4">Redeem</button>
+        <button type="button" class="btn btn-primary btn-lg mt-4" data-toggle="modal"
+          data-target="#redeemModal" data-placement="top"
+          :disabled="mobileNo.trim().length != 10"
+          @click="redeemRewardPoints">Redeem</button>
       </div>
     </form>
     <div class="row mt-5 justify-content-around">
@@ -22,7 +28,7 @@
         <div class="card">
           <img :src="amazonImg" class="card-img-top" alt="...">
           <div class="card-body">
-            <p class="card-text">Amazon Gift Cards: Buy Gift cards, gift vouchers and e-gift cards online from popular brands in India. Shop Zodiac egift cards.</p>
+            <p class="card-text">Amazon Gift Cards: Buy Gift cards, gift vouchers and e-gift cards online from popular brands in India</p>
           </div>
           <div class="card-body">
             <a href="#" class="card-link">Redeem</a>
@@ -41,6 +47,30 @@
         </div>
       </div>
     </div>
+    <div
+      v-show="showModal"
+      class="modal fade"
+      id="redeemModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="redeem modal"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            Please confirm
+          </div>
+          <div class="modal-body">
+              <label for="comment">Your are going to redeem {{redeemPoints}} and Your Mobile No is {{mobileNo}}</label>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" data-dismiss="modal" @click="confirmRedeem()">Confirm</button>
+          </div>
+        </div>
+  </div>
+  </div>
   </div>
 </template>
 <script>
@@ -61,12 +91,34 @@ export default class RedeemPoints extends Vue {
   paytmImg = ''
   amazonImg = ''
   flipkartImg = ''
+  redeemPoints =''
+  mobileNo = ''
+  isInValidPoints = false;
+  isInValidMobileNo = false;
+  showSuccessMessage = false;
+
   async created () {
-    this.pid = this.userDetails.pid;
+    this.pid = this.userDetails.pid
     this.userDetailsService.isUserAuthorized('rewards')
-    this.paytmImg = this.appImages.paytm;
-    this.amazonImg = this.appImages.amazon;
-    this.flipkartImg = this.appImages.flipkart;
+    this.paytmImg = this.appImages.paytm
+    this.amazonImg = this.appImages.amazon
+    this.flipkartImg = this.appImages.flipkart
+  }
+
+  isPointsAvailable () {
+    this.isInValidPoints = this.redeemPoints > this.totalRewardPoints
+  }
+
+  redeemRewardPoints () {
+    if (this.mobileNo.length !== 10) {
+      this.isInValidMobileNo = true
+    } else {
+      this.showModal = true
+    }
+  }
+
+  confirmRedeem () {
+    this.showSuccessMessage = true
   }
 }
 </script>
